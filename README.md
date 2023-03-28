cd09dd7c2d3b059f059790beeb598b10456664f9
SacredAntwon <antwonm9898@gmail.com>
2023-03-27 18:12:01 -0700

    Finished Project

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # CPSC_449_Midterm_Project
+
+### Members
+
+Member One: Steven Burroughs
+
+Member Two: Anthony Maida
+
+### Setting Up Virtual Environment
+
+Step 1) Clone repository and change to the directory where the files were cloned.
+
+Step 2) Create a virtual environment: "python3 -m venv env"
+
+Step 3) Activate virtual environment: "source env/bin/activate"
+
+Step 4) Install all libraries in the requirements.txt: "pip3 install -r requirements.txt"
+
+### Running the Code
+
+Step 1) Once you have the virtual environment activated in the cloned repository, run app_admin.py: "python3 app_admin.py"
+
+### Using Postman to Test
+
+Watch uploaded video for testing postman
--- app_admin.py
+++ app_admin.py
@@ -1,27 +1,36 @@
-# Store this code in 'app.py' file
-from flask import Flask, render_template, request, redirect, url_for, session, abort, jsonify
-# from  flask_mysqldb import MySQL
+# Import required libraries
+from flask import Flask, request, abort, jsonify
 import pymysql
 from flask_cors import CORS
-from datetime import timedelta
-# import MySQLdb.cursors
-import re
+import datetime
+import os
+from flask_restful import Api
+import jwt
+from functools import wraps
+from werkzeug.utils import secure_filename
 
-
-
+# Create a Flask instance
 app = Flask(__name__)
-# CORS(app)
+API = Api(app)
+
+# Configiure the file upload
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', '.pdf'] 
+app.config['UPLOAD_PATH'] = 'uploads'
+
+# Create a list of items for public access
+public = [
+    {"id": 1, "name": "Item 1", "description": "Public 1 test."},
+    {"id": 2, "name": "Item 2", "description": "Public 2 test."},
+    {"id": 3, "name": "Item 3", "description": "Public 3 test."}
+]
+
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+# Set the secret key
 app.secret_key = 'happykey'
-app.permanent_session_lifetime = timedelta(minutes=10)
 
-
-# app.config['MYSQL_HOST'] = '127.0.0.1'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = '1234'
-# app.config['MYSQL_DB'] = 'test'
-    # To connect MySQL database
+# To connect MySQL database
 conn = pymysql.connect(
         host='127.0.0.1',
         user='root', 
@@ -31,131 +40,84 @@ conn = pymysql.connect(
 
 cur = conn.cursor(cursor=pymysql.cursors.DictCursor)
 
+# Define a token_required decorator for routes requiring authentication
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user = data['username']
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+# Error handling for HTTP 400, 401, 404
+@app.errorhandler(400)
+def unauthorized_access(e):
+	return jsonify(error=str(e)), 400
+
 @app.errorhandler(401)
 def unauthorized_access(e):
-	return render_template('401.html', error=e)
-		
-@app.route('/')
-@app.route('/login', methods =['GET', 'POST'])
+	return jsonify(error=str(e)), 401
+
+@app.errorhandler(404)
+def unauthorized_access(e):
+	return jsonify(error=str(e)), 404
+
+# Route for user login
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 	msg = ''
 	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
 		username = request.form['username']
 		password = request.form['password']
-		# cursor = cur.cursor(MySQLdb.cursors.DictCursor)
-		cur.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+		cur.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
 		conn.commit()
 		account = cur.fetchone()
-		print(account)
 		if account:
-			session.permanent = True
-			session['loggedin'] = True
-			session['id'] = account['id']
-			session['username'] = account['username']
-			msg = 'Logged in successfully !'
-			return render_template('index.html', msg = msg)
+			# Generate JWT token
+			token = jwt.encode({'username': account['username'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=5)}, app.config['SECRET_KEY'], algorithm="HS256")
+			return jsonify({'token': token})
 		else:
 			msg = 'Incorrect username / password !'
-	else:
-		if 'loggedin' in session:
-			msg = 'Logged in successfully !'
-			return render_template('index.html', msg = msg)
 
-	return render_template('login.html', msg = msg)
+	return jsonify({'Invalid Password': msg})
 
-@app.route('/logout')
-def logout():
-    session.pop('loggedin', None)
-    session.pop('id', None)
-    session.pop('username', None)
-    return redirect(url_for('login'))
+# Route for file upload
+@app.route('/file', methods=['POST'])
+@token_required
+def upload_files(current_user):
+	uploaded_file = request.files['file']
+	filename = secure_filename(uploaded_file.filename) 
+	if filename != '':
+		file_ext = os.path.splitext(filename)[1]
+		if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+			abort(400) 
+		uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+	return jsonify({'message': 'File successfully uploaded'})
 
-@app.route('/admin')
-def admin():
-	if session['username'] == 'admin':
-		return render_template("admin.html")
-	else:
-		abort(401)
-		
-@app.route('/register', methods =['GET', 'POST'])
-def register():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'address' in request.form and 'city' in request.form and 'country' in request.form and 'postalcode' in request.form and 'organisation' in request.form:
-		username = request.form['username']
-		password = request.form['password']
-		email = request.form['email']
-		organisation = request.form['organisation']
-		address = request.form['address']
-		city = request.form['city']
-		state = request.form['state']
-		country = request.form['country']
-		postalcode = request.form['postalcode']
-		# cursor = cur.cursor(MySQLdb.cursors.DictCursor)
-		cur.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
-		account = cur.fetchone()
-		if account:
-			msg = 'Account already exists !'
-		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-			msg = 'Invalid email address !'
-		elif not re.match(r'[A-Za-z0-9]+', username):
-			msg = 'name must contain only characters and numbers !'
-		else:
-			cur.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s, % s, % s, % s, % s, % s, % s)', (username, password, email, organisation, address, city, state, country, postalcode, ))
-			conn.commit()
-			msg = 'You have successfully registered !'
-	elif request.method == 'POST':
-		msg = 'Please fill out the form !'
-	return render_template('register.html', msg = msg)
+# Route for public items
+@app.route('/public', methods=['GET'])
+def get_public_items():
+    return jsonify(public)
 
-
-@app.route("/index")
-def index():
-	if 'loggedin' in session:
-		return render_template("index.html")
-	return redirect(url_for('login'))
-
-
+# Route to display username based on token access
 @app.route("/display")
-def display():
-	if 'loggedin' in session:
-		# cursor = cur.cursor(MySQLdb.cursors.DictCursor)
-		cur.execute('SELECT * FROM accounts WHERE id = % s', (session['id'], ))
-		account = cur.fetchone()
-		return render_template("display.html", account = account)
-	
-	return redirect(url_for('login'))
+@token_required
+def display(current_user):
+	return jsonify(current_user)
 
-@app.route("/update", methods =['GET', 'POST'])
-def update():
-	msg = ''
-	if 'loggedin' in session:
-		if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'address' in request.form and 'city' in request.form and 'country' in request.form and 'postalcode' in request.form and 'organisation' in request.form:
-			username = request.form['username']
-			password = request.form['password']
-			email = request.form['email']
-			organisation = request.form['organisation']
-			address = request.form['address']
-			city = request.form['city']
-			state = request.form['state']
-			country = request.form['country']
-			postalcode = request.form['postalcode']
-			# cursor = cur.cursor(MySQLdb.cursors.DictCursor)
-			cur.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
-			account = cur.fetchone()
-			if account:
-				msg = 'Account already exists !'
-			elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-				msg = 'Invalid email address !'
-			elif not re.match(r'[A-Za-z0-9]+', username):
-				msg = 'name must contain only characters and numbers !'
-			else:
-				cur.execute('UPDATE accounts SET username =% s, password =% s, email =% s, organisation =% s, address =% s, city =% s, state =% s, country =% s, postalcode =% s WHERE id =% s', (username, password, email, organisation, address, city, state, country, postalcode, (session['id'], ), ))
-				# cur.commit()
-				msg = 'You have successfully updated !'
-		elif request.method == 'POST':
-			msg = 'Please fill out the form !'
-		return render_template("update.html", msg = msg)
-	return redirect(url_for('login'))
 
 if __name__ == "__main__":
 	app.run(host ="127.0.0.1")
